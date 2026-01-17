62964ac9990472401799e6051c6605a66f49331e
lehoangtrieuvy15@gmail.com <trieuvy.lehoang@beink.fr>
2026-01-17 13:51:52 +0100

    Update agents/__init__.py to include ChartDataAgent

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,5 +2,7 @@
 
 from .base_agent import BaseAgent
 from .news_agent import NewsAgent
+from .report_agent import ReportAnalysisAgent
+from .chart_agent import ChartDataAgent
 
-__all__ = ["BaseAgent", "NewsAgent"]
+__all__ = ["BaseAgent", "NewsAgent", "ReportAnalysisAgent", "ChartDataAgent"]
