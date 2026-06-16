78e64db37d4b2253c2a75dd80f9a33889df1bc23
lehoangtrieuvy15@gmail.com <trieuvy.lehoang@beink.fr>
2026-06-16 11:01:34 +0200

    reinit

--- src/core/base/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Base classes for all Event Horizon components"""
-
-from event_horizon.core.base.base_agent import BaseAgent
-from event_horizon.core.base.base_orchestrator import BaseOrchestrator
-
-__all__ = ["BaseAgent", "BaseOrchestrator"]
